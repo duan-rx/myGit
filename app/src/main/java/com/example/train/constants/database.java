d113216828c0e9bae8a098711e7acde752c97bca
duan-rx <2144642453@qq.com>
2020-08-11 21:20:56 +0800

    删除app和train

--- app/src/main/java/com/example/train/constants/database.java
+++
@@ -1,6 +0,0 @@
-package com.example.train.constants;
-
-public interface database {
-    public static final  String DATABASE_NAME="TRAIN";
-    public static final int VERSION_CODE=3;
-}
--- app/src/main/java/com/example/train/constants/message.java
+++
@@ -1,8 +0,0 @@
-package com.example.train.constants;
-
-public class message {
-    public static  String message="";
-    public static  String message2="";
-    //public final static String path="http://192.168.1.103:8080";
-    public final static String path="http://47.96.184.211:8080";
-}
